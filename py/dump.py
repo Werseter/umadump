45bd89632d45cc8f0a17a95f7504ba9e95dd28f4
Werseter <werseter@interia.eu>
2025-12-17 21:09:46 +0100

    Update parent data extractor

--- py/dump.py
+++ py/dump.py
@@ -34,13 +34,16 @@ def on_message(message, data):
                 case "parent_borrows_data":
                     print("Received parent borrows data...")
                     data_parts["parent_borrows_data"] = data
+                case "parent_borrows_extra_data":
+                    print("Received parent borrows extra data...")
+                    data_parts["parent_borrows_extra_data"] = data
                 case _:
                     print(f"Unknown payload: {payload}")
             if "veteran_data" in data_parts and "veteran_extra_data" in data_parts:
-                print("Both data parts received, merging and extracting...")
+                print("Both veteran data parts received, merging and extracting...")
                 extract_veteran_data()
-            if "parent_borrows_data" in data_parts:
-                print("Parent borrows data received.")
+            if "parent_borrows_data" in data_parts and "parent_borrows_extra_data" in data_parts:
+                print("Both parent data parts received, merging and extracting...")
                 extract_parent_borrows_data()
         case "error":
             print(message.get('stack'))
@@ -75,15 +78,28 @@ def extract_veteran_data():
         # mark veteran extractor as successful
         extraction_status['veteran'] = True
 
+
 def extract_parent_borrows_data():
     try:
         with open("parent_borrows_data.msgpack", "wb+") as f:
             f.write(data_parts["parent_borrows_data"])
 
+        with open("parent_borrows_extra_data.msgpack", "wb+") as f:
+            f.write(data_parts["parent_borrows_extra_data"])
+
         parent_borrows_data = msgpack.loads(data_parts["parent_borrows_data"])
+        parent_borrows_extra_data = msgpack.loads(data_parts["parent_borrows_extra_data"])
+
+        # Append username from extra using 'viewer_id'
+        merged = []
+        for item in parent_borrows_data:
+            viewer_id = item['viewer_id']
+            extra = next((x for x in parent_borrows_extra_data if x['viewer_id'] == viewer_id), {})
+            merged_item = {'name': extra['name'], **item}
+            merged.append(merged_item)
 
         with open("umadump_parents_data.json", "w+") as f:
-            f.write(json.dumps(parent_borrows_data, indent=2))
+            f.write(json.dumps(merged, indent=2))
     except Exception:
         raise
     else:
@@ -91,16 +107,18 @@ def extract_parent_borrows_data():
         extraction_status['parent_borrows'] = True
 
 script = session.create_script(r"""
-//                                  'B3 trained_chara_array'
-const trainedCharaPattern =         'B3 74 72 61 69 6E 65 64 5F 63 68 61 72 61 5F 61 72 72 61 79';
-//                                  'BC trained_chara_favorite_array'
-const trainedCharaFavoritePattern = 'BC 74 72 61 69 6E 65 64 5F 63 68 61 72 61 5F 66 61 76 6F 72 69 74 65 5F 61 72 72 61 79';
-//                                  'BF room_match_entry_chara_id_array'
-const roomMatchEntryPattern =       'BF 72 6F 6F 6D 5F 6D 61 74 63 68 5F 65 6E 74 72 79 5F 63 68 61 72 61 5F 69 64 5F 61 72 72 61 79'
-//                                  'B7 summary_user_info_array'
-const summaryUserInfoPattern =      'B7 73 75 6D 6D 61 72 79 5F 75 73 65 72 5F 69 6E 66 6F 5F 61 72 72 61 79';
-//                                  'B7 support_card_data_array'
-const supportCardDataPatten =       'B7 73 75 70 70 6F 72 74 5F 63 61 72 64 5F 64 61 74 61 5F 61 72 72 61 79';
+//                                    'B3 trained_chara_array'
+const trainedCharaPattern =           'B3 74 72 61 69 6E 65 64 5F 63 68 61 72 61 5F 61 72 72 61 79';
+//                                    'BC trained_chara_favorite_array'
+const trainedCharaFavoritePattern =   'BC 74 72 61 69 6E 65 64 5F 63 68 61 72 61 5F 66 61 76 6F 72 69 74 65 5F 61 72 72 61 79';
+//                                    'BF room_match_entry_chara_id_array'
+const roomMatchEntryPattern =         'BF 72 6F 6F 6D 5F 6D 61 74 63 68 5F 65 6E 74 72 79 5F 63 68 61 72 61 5F 69 64 5F 61 72 72 61 79'
+//                                    'BE succession_trained_chara_array'
+const successionTrainedCharaPattern = 'BE 73 75 63 63 65 73 73 69 6F 6E 5F 74 72 61 69 6E 65 64 5F 63 68 61 72 61 5F 61 72 72 61 79';
+//                                    'B7 summary_user_info_array'
+const summaryUserInfoPattern =        'B7 73 75 6D 6D 61 72 79 5F 75 73 65 72 5F 69 6E 66 6F 5F 61 72 72 61 79';
+//                                    'B7 support_card_data_array'
+const supportCardDataPatten =         'B7 73 75 70 70 6F 72 74 5F 63 61 72 64 5F 64 61 74 61 5F 61 72 72 61 79';
 
 function findPayload(startPattern, endPattern, tag) {
     const ranges = Process.enumerateRanges({protection: "r--", coalesce: true});
@@ -122,7 +140,8 @@ function findPayload(startPattern, endPattern, tag) {
 
 findPayload(trainedCharaPattern, trainedCharaFavoritePattern, "veteran_data");
 findPayload(trainedCharaFavoritePattern, roomMatchEntryPattern, "veteran_extra_data");
-findPayload(summaryUserInfoPattern, supportCardDataPatten, "parent_borrows_data");
+findPayload(successionTrainedCharaPattern, summaryUserInfoPattern, "parent_borrows_data");
+findPayload(summaryUserInfoPattern, supportCardDataPatten, "parent_borrows_extra_data");
 """)
 script.on("message", on_message)
 script.load()
