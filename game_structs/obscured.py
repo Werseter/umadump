35b2cfdb425c47922d7bc0762ce69f100cfb3e7d
Werseter <werseter@interia.eu>
2026-09-12 23:37:18 +0200

    Handle uninitialized Obscured values

--- game_structs/obscured.py
+++ game_structs/obscured.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from ctypes import c_bool, c_int32, c_int64, c_uint64, c_uint8
 
-from ctypes_utils import CStructureDataclass, C_Int, C_Ptr, PointerWrapperMixin
+from ctypes_utils import CStructureDataclass, C_Bool, C_Int, C_Ptr, PointerWrapperMixin
 from game_structs.collections import GenericArrayPtr
 from game_structs.strings import SystemStringObjectPtr
 from il2cpp_structs import RuntimeIl2CppObject
@@ -15,12 +15,14 @@ from il2cpp_structs import RuntimeIl2CppObject
 class ObscuredBool(CStructureDataclass):
     currentCryptoKey: C_Int[c_uint8]
     hiddenValue: C_Int[c_int32]
-    _ignored_1: c_bool  # omitted: inited
-    _ignored_2: c_bool  # omitted: fakeValue
-    _ignored_3: c_bool  # omitted: fakeValueActive
+    inited: C_Bool[c_bool]
+    _ignored_1: c_bool  # omitted: fakeValue
+    _ignored_2: c_bool  # omitted: fakeValueActive
 
     @property
     def value(self) -> bool:
+        if not self.inited:
+            return False
         false_sentinel = 0xB5
         decoded = int(self.currentCryptoKey) ^ int(self.hiddenValue)
         return decoded != false_sentinel
@@ -29,36 +31,42 @@ class ObscuredBool(CStructureDataclass):
 class ObscuredInt(CStructureDataclass):
     currentCryptoKey: C_Int[c_int32]
     hiddenValue: C_Int[c_int32]
-    _ignored_1: c_bool  # omitted: inited
-    _ignored_2: c_int32  # omitted: fakeValue
-    _ignored_3: c_bool  # omitted: fakeValueActive
+    inited: C_Bool[c_bool]
+    _ignored_1: c_int32  # omitted: fakeValue
+    _ignored_2: c_bool  # omitted: fakeValueActive
 
     @property
     def value(self) -> int:
+        if not self.inited:
+            return 0
         return int(self.currentCryptoKey) ^ int(self.hiddenValue)
 
 
 class ObscuredLong(CStructureDataclass):
     currentCryptoKey: C_Int[c_int64]
     hiddenValue: C_Int[c_int64]
-    _ignored_1: c_bool  # omitted: inited
-    _ignored_2: c_int64  # omitted: fakeValue
-    _ignored_3: c_bool  # omitted: fakeValueActive
+    inited: C_Bool[c_bool]
+    _ignored_1: c_int64  # omitted: fakeValue
+    _ignored_2: c_bool  # omitted: fakeValueActive
 
     @property
     def value(self) -> int:
+        if not self.inited:
+            return 0
         return int(self.currentCryptoKey) ^ int(self.hiddenValue)
 
 
 class ObscuredULong(CStructureDataclass):
     currentCryptoKey: C_Int[c_uint64]
     hiddenValue: C_Int[c_uint64]
-    _ignored_1: c_bool  # omitted: inited
-    _ignored_2: c_uint64  # omitted: fakeValue
-    _ignored_3: c_bool  # omitted: fakeValueActive
+    inited: C_Bool[c_bool]
+    _ignored_1: c_uint64  # omitted: fakeValue
+    _ignored_2: c_bool  # omitted: fakeValueActive
 
     @property
     def value(self) -> int:
+        if not self.inited:
+            return 0
         return int(self.currentCryptoKey) ^ int(self.hiddenValue)
 
 
@@ -66,12 +74,14 @@ class ObscuredString(CStructureDataclass):
     _il2cpp_obj: RuntimeIl2CppObject
     currentCryptoKey: SystemStringObjectPtr
     hiddenValue: GenericArrayPtr[c_uint8]
-    _ignored_1: c_bool  # omitted: inited
-    _ignored_2: SystemStringObjectPtr  # omitted: fakeValue
-    _ignored_3: c_bool  # omitted: fakeValueActive
+    inited: C_Bool[c_bool]
+    _ignored_1: SystemStringObjectPtr  # omitted: fakeValue
+    _ignored_2: c_bool  # omitted: fakeValueActive
 
     @property
     def value(self) -> str:
+        if not self.inited:
+            return ''
         key_str = self.currentCryptoKey.value
         key_len = len(key_str)
         if key_len == 0:
